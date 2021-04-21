93894c568f8ceb14f96b5f7a456988af54050d1b
jay <jaybarria@gmail.com>
2021-04-21 21:44:35 +0800

    Koin Setup

--- app/src/main/java/com/github/jairrab/koinexercise/MainActivity.kt
+++ app/src/main/java/com/github/jairrab/koinexercise/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.github.jairrab.koinexercise
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
 
 class MainActivity : AppCompatActivity() {
+    // Lazy injected MySimplePresenter
+    private val firstPresenter: MySimplePresenter by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.v("koin_test", firstPresenter.sayHello())
+    }
+}
+
+interface HelloRepository {
+    fun giveHello(): String
+}
+
+class HelloRepositoryImpl() : HelloRepository {
+    override fun giveHello() = "Hello Koin"
+}
+
+class MySimplePresenter(private val repo: HelloRepository) {
+    fun sayHello() = "${repo.giveHello()} from $this"
+}
+
+object Modules {
+    val appModule = module {
+        // single instance of HelloRepository
+        single<HelloRepository> { HelloRepositoryImpl() }
+
+        // Simple Presenter Factory
+        factory { MySimplePresenter(get()) }
+    }
+}
+
+class MyApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        // Start Koin
+        startKoin{
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(Modules.appModule)
+        }
     }
 }
