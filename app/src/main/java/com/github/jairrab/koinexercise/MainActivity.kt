a5cb4f54343fd4662913c5d2f5e9f41ec62096a5
jay <jaybarria@gmail.com>
2021-04-21 22:17:59 +0800

    Scoping injectables into Activity

--- app/src/main/java/com/github/jairrab/koinexercise/MainActivity.kt
+++ app/src/main/java/com/github/jairrab/koinexercise/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.github.jairrab.koinexercise
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.scope.activityScope
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
 //region APPLICATION
-class MyApplication : Application(){
+class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Start Koin
-        startKoin{
+        startKoin {
             androidLogger()
             androidContext(this@MyApplication)
             modules(Modules.appModule)
@@ -25,7 +30,9 @@ class MyApplication : Application(){
 //endregion
 
 //region MAIN ACTIVITY
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AndroidScopeComponent {
+    override val scope: Scope by activityScope()
+
     // Lazy injected MySimplePresenter
     private val firstPresenter: MySimplePresenter by inject()
 
@@ -34,7 +41,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.v("koin_test", firstPresenter.sayHello())
+
+        findViewById<TextView>(R.id.text).text = firstPresenter.sayHello()
     }
+}
+//endregion
+
+class MyViewModel : ViewModel() {
+
+}
+
+//region PRESENTER
+class MySimplePresenter(private val repo: HelloRepository) {
+    fun sayHello() = "${repo.giveHello()} from $this"
 }
 //endregion
 
@@ -48,20 +67,19 @@ class HelloRepositoryImpl() : HelloRepository {
 }
 //endregion
 
-//region PRESENTER
-class MySimplePresenter(private val repo: HelloRepository) {
-    fun sayHello() = "${repo.giveHello()} from $this"
-}
-//endregion
-
 //region KOIN MODULES
 object Modules {
     val appModule = module {
         // single instance of HelloRepository
         single<HelloRepository> { HelloRepositoryImpl() }
 
-        // Simple Presenter Factory
-        factory { MySimplePresenter(get()) }
+        //factory - to produce a new instance each time the by inject() or get() is called
+        //factory { MySimplePresenter(get()) }
+
+        //scope - to produce an instance tied to a scope
+        scope<MainActivity> {
+            scoped { MySimplePresenter(get()) }
+        }
     }
 }
 //endregion
